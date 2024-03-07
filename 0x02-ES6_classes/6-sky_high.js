f2b15603be2465fa769243b893bcd8b682fea860
amusukwa <amusukwa@gmail.com>
2024-03-07 21:55:09 +0000

    7-main.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new Error('Floors must be a number.');
+    }
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new Error('Floors must be a number.');
+    }
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
