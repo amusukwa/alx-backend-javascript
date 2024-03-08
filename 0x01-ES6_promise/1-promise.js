34acb324f9ceb614faa320fc498e3d4f8f6c3b81
amusukwa <amusukwa@gmail.com>
2024-03-08 11:23:55 +0000

    1-promise.js error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (success === true) {
-        const response = {
-          status: 200,
-          data: 'Some data from the API'
-        };
-        resolve(response);
-      } else {
-        reject('The fake API is not working currently');
-      }
-    }, 1000);
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
 }
