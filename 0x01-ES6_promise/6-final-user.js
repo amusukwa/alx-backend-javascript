33fd4d09a85d821306c6a454f9c4d2556f4c83c1
amusukwa <amusukwa@gmail.com>
2024-03-08 12:57:04 +0000

    6-final-user.js error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,24 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then(results => {
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      }));
-    });
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      {
+        status: userResult.status,
+        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
+      },
+      {
+        status: photoResult.status,
+        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
+      },
+    ];
+  } catch (error) {
+    return [{ status: 'rejected', value: error }];
+  }
 }
