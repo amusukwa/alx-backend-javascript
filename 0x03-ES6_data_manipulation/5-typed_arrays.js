2ae3786e91535316ba87e255ab333d5c9f55e19c
amusukwa <amusukwa@gmail.com>
2024-03-12 13:04:36 +0000

    6-set.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
-export default createInt8TypedArray(length, position, value){
-if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
-    }
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const buffer = new ArrayBuffer(length);
-    const view = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
 
-    view[position] = value;
+  view[position] = value;
 
-    return new DataView(buffer);
+  return new DataView(buffer);
 }
