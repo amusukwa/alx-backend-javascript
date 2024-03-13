f304eea9d38538b69d275f12a84a58fd83cc8932
amusukwa <amusukwa@gmail.com>
2024-03-13 21:24:32 +0000

    task 10

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,26 @@
+// Import necessary modules
+import { Subjects } from './subjects';
+
+// Create and export constants for Cpp, Java, and React subjects
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Create and export a Teacher object with experienceTeachingC = 10
+export const cTeacher: Subjects.Teacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
