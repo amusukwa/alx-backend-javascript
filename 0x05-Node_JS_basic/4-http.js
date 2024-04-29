6359339ec35a7136305c5be85f467b025e94d6a0
amusukwa <amusukwa@gmail.com>
2024-04-29 13:40:34 +0000

    4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+// Create the HTTP server
+const app = http.createServer((req, res) => {
+    // Set the response status and headers
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Send the response body
+    res.end('Hello Holberton School!\n');
+});
+
+app.listen(1245);
+
+// Export the app variable
+module.exports = app;
