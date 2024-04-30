bdc95b0a158847c419218a2220c47e4a7762a75e
amusukwa <amusukwa@gmail.com>
2024-04-30 17:36:38 +0000

    7-http_express.js 3

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,6 +3,7 @@ const { countStudents } = require('./3-read_file_async');
 
 const app = express();
 
+// Define a route handler for the root path
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!\n');
 });
@@ -12,12 +13,13 @@ app.get('/students', (req, res) => {
   // Call countStudents function passing the database file as argument
   countStudents(process.argv[2])
     .then((message) => {
-      res.send('This is a list of students\n${message}');
+      res.send(`This is a list of students\n${message}`);
     })
     .catch((error) => {
-      res.send('This is the list of our sudents\n${error.message}');
+      res.send(`This is the list of our students\n${error.message}`);
     });
 });
+
 app.listen(1245);
 
 module.exports = app;
