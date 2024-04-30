fa854c0667e3c021aeeebec41b99e33d78045fd2
amusukwa <amusukwa@gmail.com>
2024-04-30 12:33:16 +0000

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,13 +2,18 @@
 
 const http = require('http');
 
-const PORT = 3000;
+const PORT = 1245;
 
+// Create the HTTP server
 const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!\n');
 });
 
+// Export the server
+module.exports = app;
+
+// Listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
