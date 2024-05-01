fed1bebeac6370d19edf0bb05a2c9096cf0b6154
amusukwa <amusukwa@gmail.com>
2024-05-01 20:52:37 +0000

    10-api/

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,30 @@
+// api.js
+
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
+});
+
+const server = app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = server;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,33 @@
+// api.test.js
+
+const request = require('request');
+const { expect } = require('chai');
+const server = require('./api');
+
+describe('Login endpoint', function() {
+  it('should return correct message with username', function(done) {
+    request.post({
+      url: 'http://localhost:7865/login',
+      json: { userName: 'Betty' }
+    }, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
+  });
+});
+
+describe('Available payments endpoint', function() {
+  it('should return correct payment methods', function(done) {
+    request.get('http://localhost:7865/available_payments', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(JSON.parse(body)).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
+      done();
+    });
+  });
+});
